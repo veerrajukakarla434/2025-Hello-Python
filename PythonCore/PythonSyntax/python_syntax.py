b697da0115cd91366ba044365af4499cf5e3da08
veerrajukakarla434 <kakarlaveerraju2010@gmail.com>
2025-08-01 09:05:47 +0530

    Python Synatax Changes

--- PythonCore/PythonSyntax/python_syntax.py
+++ PythonCore/PythonSyntax/python_syntax.py
@@ -3,3 +3,19 @@ if 10>5:  # python syntax code
     print("I am tab indentation")
     print("I am tab indentation 2 OR same block of code") #block of code ended
 print("I am out of indentation Or out of the block")    # this statement is out of the if block
+
+# OR We can write like this
+if 10 > 5:
+    print("This is true!")
+    print("I am tab indentation")
+                                    # giving spca for redability neline  code will be out of if block
+print("I have no indentation")
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+"""
+if 5 > 2:
+print("Five is greater than two!") # Syntax error   need to give minimum one space  and block of  statements comment
+"""
+print("Hello, World!") 
