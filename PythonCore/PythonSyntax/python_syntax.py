e7c28ba1d20d19774e6be30b9ed587b2388a0f71
veerrajukakarla434 <kakarlaveerraju2010@gmail.com>
2025-08-01 08:57:02 +0530

    Python Synatax Changes

---
+++ PythonCore/PythonSyntax/python_syntax.py
@@ -0,0 +1,5 @@
+if 10>5:
+    print("This is true")
+    print("I am tab indentation")
+    print("I am tab indentation 2 OR same block of code")
+print("I am out of indentation Or out of the block")    
