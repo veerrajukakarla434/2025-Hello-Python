5af164f6311aed64f4562ff1ed0e15fde1138ff8
veerrajukakarla434 <kakarlaveerraju2010@gmail.com>
2025-08-02 22:12:11 +0530

    Added Input and out put in python

---
+++ PythonCore/Input-and-Output-in-Python/input_output_in_python.py
@@ -0,0 +1,26 @@
+# Taking input from console
+
+name = input("Enter the String")
+print("Welcome to "+name+" World class Python Programming")
+
+# Single variable
+s = "Bob"
+print(s)
+
+# Multiple Variables
+s = "Alice"
+age = 25
+city = "New York"
+print(s, age, city)
+
+# Take Multiple Input in Python
+# taking two inputs at a time
+x, y = input("Enter two values: ").split()
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+ 
+# taking three inputs at a time
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
