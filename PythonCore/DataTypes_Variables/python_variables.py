8e8e1574b5dfa8e7c21bed1e18185c21cf70faa5
veerrajukakarla434 <kakarlaveerraju2010@gmail.com>
2025-08-02 22:34:18 +0530

    Python variable added

---
+++ PythonCore/DataTypes_Variables/python_variables.py
@@ -0,0 +1,47 @@
+# Variable 'x' stores the integer value 10
+x = 5
+
+# Variable 'name' stores the string "Samantha"
+name = "Samantha"  
+
+print(x)
+print(name)
+
+# Valid Example:
+
+age = 21
+_colour = "lilac"
+total_score = 90
+
+# Invalid Example:
+# 1name = "Error"  # Starts with a digit (added comment becuase giving error , to check this you can remove starting #)
+# class = 10       # 'class' is a reserved keyword
+# user-name = "Doe"  # Contains a hyphen
+
+# Basic Assignment
+x = 5
+y = 3.14
+z = "Hi"
+
+# Dynamic Typing
+x = 10
+x = "Now a string"
+
+# Assigning Different Values
+x, y, z = 1, 2.5, "Python"
+print(x, y, z)
+
+# Type Casting a Variable
+
+# Casting variables
+s = "10"  # Initially a string
+n = int(s)  # Cast string to integer
+cnt = 5
+f = float(cnt)  # Cast integer to float
+age = 25
+s2 = str(age)  # Cast integer to string
+
+# Display results
+print(n)  
+print(f)  
+print(s2)
